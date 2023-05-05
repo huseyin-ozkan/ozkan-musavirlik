f380cf61a46549018f50aa4c1646120c9dca5e83
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-05-05 20:28:58 +0300

    build: setup SASS

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const filePath = dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess({
+		scss: {
+			prependData: `@import '${filePath.replaceAll('\\', '/')}/src/lib/styles/app.scss';`
+		}
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
