f8291f74f13ab63c3f5b0fe8af3b0ed8ddccfdfc
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-05-26 20:20:34 +0300

    feat: public blog section

---
+++ src/lib/components/PostPreview.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	type Post = {
+		id: number
+		createdAt: Date
+		updatedAt: Date
+
+		title: string
+		content: string
+	}
+
+	// <!-- TODO remove mock
+	export const post: Post = {
+		id: 1,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+		title: '509 Sıra No.lu Vergi Usul Kanunu Genel Tebliği’nde Değişiklik',
+		content: `Elektronik belge uygulamalarının yaygınlaştırılması, mükelleflerin vergiye uyum maliyetlerinin
+			azaltılması ve kayıt dışı ekonomi ile mücadeledeki etkinliğin artırılması amacıyla, 19/10/2019
+			tarihli ve 30923 sayılı Resmî Gazete’de yayımlanan509 Sıra No.lu Vergi Usul Kanunu Genel
+			Tebliğinde değişiklik yapılmak suretiyle`
+	}
+</script>
+
+<a href="/posts/{post.id}" title="Devamını oku">
+	<article>
+		<!-- TODO format to Turkish locale DD MMMM YYYY -->
+		<span class="date"> {post.createdAt.toLocaleDateString()} </span>
+		<h1>{post.title}</h1>
+		<p>
+			{post.content}...
+		</p>
+	</article>
+</a>
+
+<style lang="scss">
+	article {
+		--color-date: #6a93ff;
+		--color-p: #7988bb;
+
+		--h1-font-size: clamp(1.2rem, 1.5vw, 1.4rem);
+		--date-font-size: clamp(0.7rem, 1vw, 0.9rem);
+		--p-font-size: clamp(0.85rem, 1.2vw, 1rem);
+
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		justify-content: start;
+
+		width: 100%;
+		height: 100%;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	// underline on hover
+	a:hover {
+		text-decoration: underline;
+	}
+
+	.date {
+		font-size: var(--date-font-size);
+		font-weight: 700;
+		color: var(--color-date);
+		margin-bottom: 0.5em;
+	}
+
+	h1 {
+		font-size: var(--h1-font-size);
+		font-weight: 600;
+		font-family: Ubuntu, Mulish, sans-serif;
+		margin-bottom: 1em;
+	}
+
+	p {
+		font-size: var(--p-font-size);
+		font-weight: 400;
+		color: var(--color-p);
+		font-family: Ubuntu, Mulish, sans-serif;
+	}
+</style>
--- src/routes/Blog.svelte
+++ src/routes/Blog.svelte
@@ -1,1 +1,139 @@
-<!-- TODO blog section -->
+<script lang="ts">
+	import 'swiper/css'
+	import 'swiper/css/autoplay'
+	import 'swiper/css/pagination'
+	import { Swiper, SwiperSlide } from 'swiper/svelte'
+	import { Pagination, Autoplay } from 'swiper'
+
+	import PostPreview from '$lib/components/PostPreview.svelte'
+
+	// <!-- TODO remove mock and fetch from server -->
+	const posts = Array.from({ length: 20 }, (_, i) => i + 1)
+		.map((i) => ({
+			id: i,
+			title: `Post ${i}`
+		}))
+		.reduce((prev, cur) => {
+			// paginate to 6 posts per page
+			if (prev.length === 0 || prev[prev.length - 1].length === 6) prev.push([])
+
+			prev[prev.length - 1].push(cur)
+
+			return prev
+		}, [] as any[][])
+</script>
+
+<section id="#blog">
+	<div class="pagination">
+		<Swiper
+			spaceBetween={10}
+			slidesPerView={1}
+			modules={[Pagination, Autoplay]}
+			pagination={{ clickable: true, type: 'bullets' }}
+		>
+			{#each posts as page}
+				<SwiperSlide>
+					<div class="page">
+						{#each page as post}
+							<PostPreview {post} />
+						{/each}
+					</div>
+				</SwiperSlide>
+			{/each}
+		</Swiper>
+	</div>
+	<div class="mobile-view">
+		{#each posts[0] as post}
+			<PostPreview {post} />
+		{/each}
+	</div>
+</section>
+
+<style lang="scss">
+	@mixin breakpoint() {
+		@include md {
+			@content;
+		}
+	}
+	section {
+		--section-bg: var(--color-bg-medium);
+		--pagination-bullet: #9db8ff;
+
+		--section-pv: clamp(20px, 5vw, 100px);
+		--section-border-radius: var(--section-pv);
+		--column-gap: clamp(20px, 4vw, 65px);
+		--row-gap: calc(var(--column-gap) * 0.7);
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		padding-top: var(--section-pv);
+		padding-bottom: calc(var(--section-pv) / 2); // other half is coming from pagination gap
+		border-radius: var(--section-border-radius) 0 var(--section-border-radius) 0;
+
+		background-color: var(--section-bg);
+	}
+
+	// Mobile view
+	.mobile-view {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: var(--row-gap);
+
+		@include breakpoint() {
+			display: none;
+		}
+	}
+
+	// Pagination on desktop
+	.pagination {
+		width: 100%;
+		display: none;
+
+		@include breakpoint() {
+			display: block;
+		}
+	}
+
+	// Swiper slide
+	.pagination :global(.swiper-slide) {
+		margin-bottom: calc(var(--section-pv) / 2 + 30px);
+		width: 100%;
+		height: 100%;
+	}
+
+	.page {
+		--grid-columns: 1;
+		--grid-rows: 6;
+
+		@include md {
+			--grid-columns: 2;
+			--grid-rows: 3;
+		}
+		@include xl {
+			--grid-columns: 3;
+			--grid-rows: 2;
+		}
+
+		display: grid;
+		grid-template-columns: repeat(var(--grid-columns), 1fr);
+		grid-template-rows: repeat(var(--grid-rows), 1fr);
+		column-gap: var(--column-gap);
+		row-gap: var(--row-gap);
+	}
+
+	// Pagination bullets
+	.pagination :global(.swiper-pagination-bullet) {
+		width: 1rem;
+		height: 1rem;
+		background-color: var(--pagination-bullet);
+		opacity: 0.3;
+	}
+	.pagination :global(.swiper-pagination-bullet-active) {
+		opacity: 1;
+	}
+</style>
