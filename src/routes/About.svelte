68c29c60be7b0cdecf9e0514d052f73c0d4ab761
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-05-27 22:37:22 +0300

    feat(css): about section first part styles

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -65,3 +65,92 @@
 	</section>
 </section>
 
+<style lang="scss">
+	#about {
+		--color-paragraph: #5b6c98;
+		--color-mission-vision-bg: var(--color-bg-medium);
+		--color-mission-vision-text: #7a88ba;
+
+		--section-pv: clamp(75px, 10vw, 250px);
+		--paragraph-max-width: 1000px;
+
+		--heading-font-size: clamp(1.7rem, 5vw, 4rem);
+		--paragraph-font-size: clamp(1rem, 2vw, 1.3rem);
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		padding-top: var(--section-pv);
+		padding-bottom: var(--section-pv);
+	}
+
+	#about > h1 {
+		font-size: var(--heading-font-size);
+		font-family: Mulish, Ubuntu, sans-serif;
+		font-weight: bold;
+
+		margin-bottom: 1.2em;
+	}
+
+	#about > p {
+		font-size: var(--paragraph-font-size);
+		font-family: Mulish, Ubuntu, sans-serif;
+		font-weight: 500;
+		line-height: 116%;
+		text-align: center;
+		color: var(--color-paragraph);
+
+		max-width: var(--paragraph-max-width);
+		margin-bottom: 4em;
+	}
+
+	// Mission & Vision
+	.mission-vision {
+		--title-size: clamp(1.25rem, 2.5vw, 2rem);
+		--text-size: clamp(0.95rem, calc(var(--title-size) * 0.6), 1.1rem);
+		--gap: calc(var(--title-size) * 1.4);
+
+		display: flex;
+		flex-direction: column;
+		align-items: stretch;
+		justify-content: center;
+		gap: var(--gap);
+
+		@include md {
+			flex-direction: row;
+		}
+
+		margin-bottom: 2em;
+		max-width: var(--paragraph-max-width);
+	}
+
+	.mission-vision > article {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		background-color: var(--color-mission-vision-bg);
+		border-radius: 30px;
+		padding: var(--gap);
+	}
+
+	.mission-vision > article > h2 {
+		font-size: var(--title-size);
+		font-family: Mulish, Ubuntu, sans-serif;
+		font-weight: 700;
+		margin-bottom: 0.8em;
+	}
+
+	.mission-vision > article > p {
+		font-size: var(--text-size);
+		font-family: Ubuntu, sans-serif;
+		font-weight: 400;
+		line-height: 130%;
+		text-align: center;
+		color: var(--color-mission-vision-text);
+	}
+</style>
