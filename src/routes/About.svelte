d401f3eb436b2d109d1a0f8762e2c03b824b5400
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-05-28 15:47:25 +0300

    feat: global staff section
    separated from about section and styled

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -8,30 +8,6 @@
 
 	const vision =
 		'Globalleşen dünya üzerinde kurumumuz ve müşterilerimizi bilgi düzeyi açısından en üst düzeye taşıyarak ekonomik açıdan kuvvetli, sistemli ve uzun ömürlü oluşumlar meydana getirmek; muhasebe ve mali müşavirlik hizmetleri açısından Türkiye’de lider kuruluşlardan biri olmaktır.'
-
-	type Staff = {
-		name: string
-		title: string
-		image: string
-		description: string
-	}
-
-	const staff: Staff[] = [
-		{
-			name: 'Hüseyin Özkan',
-			title: 'Serbest Muhasebeci Mali Müşavir, Gayrimenkul Değerleme Uzmanı',
-			image: './assets/mock-huseyin-ozkan.png',
-			description:
-				'2012 yılından itibaren Yahyalı ve Kayseri’de Serbest Muhasebeci Mali Müşavirlik faaliyeti yürütmekteyiz. SPK lisanslı Gayrimenkul Değerleme Uzmanı olan Hüseyin Özkan tarafından talep üzerine özel veya kamu otoritelerine verilmek üzere Gayrimenkul Ekspertiz Raporlama faaliyetinde bulunmaktadır.'
-		},
-		{
-			name: 'Hatice Banu Özkan',
-			title: 'Serbest Muhasebeci Mali Müşavir',
-			image: './assets/mock-hatice-banu-ozkan.png',
-			description:
-				'2012 yılından itibaren Yahyalı ve Kayseri’de Serbest Muhasebeci Mali Müşavirlik faaliyeti yürütmekteyiz. SPK lisanslı Gayrimenkul Değerleme Uzmanı olan Hüseyin Özkan tarafından talep üzerine özel veya kamu otoritelerine verilmek üzere Gayrimenkul Ekspertiz Raporlama faaliyetinde bulunmaktadır.'
-		}
-	]
 </script>
 
 <section id="about">
@@ -50,19 +26,6 @@
 		</article>
 	</section>
 
-	<section class="staff">
-		{#each staff as { name, title, image, description }}
-			<figure>
-				<img src={image} alt={name} />
-
-				<figcaption>
-					<span>{title}</span>
-					<h3>{name}</h3>
-					<p>{description}</p>
-				</figcaption>
-			</figure>
-		{/each}
-	</section>
 </section>
 
 <style lang="scss">
