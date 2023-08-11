354f49600a0b58574e4593e8962e01b5d9db0855
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-08-11 23:29:18 +0300

    chore(types): add SanityPost type

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -6,5 +6,31 @@ declare type Post = {
 	updatedAt: Date
 
 	title: string
+	/** HTML */
 	body: string
+	mainImage?: string
+}
+
+declare type SanityPost = {
+	_id: string
+	_rev: string
+	_type: 'post'
+	_createdAt: string
+	_updatedAt: string
+
+	title: string
+	slug: {
+		current: string
+		_type: 'slug'
+	}
+	/** Markdown */
+	body: string
+
+	mainImage?: {
+		_type: 'image'
+		asset: {
+			_ref: string
+			_type: 'reference'
+		}
+	}
 }
