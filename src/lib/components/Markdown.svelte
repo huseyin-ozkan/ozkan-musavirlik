3cd8b68289f72779281560b89510a743d4755569
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-08-15 23:27:41 +0300

    feat(components): Markdown rendering component
    -

---
+++ src/lib/components/Markdown.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let content: string = `# Hello World`
+</script>
+
+<!-- @component 
+  Display given markdown HTML content with styles
+-->
+
+<div class="markdown">
+	{@html content}
+</div>
+
+<style lang="scss">
+	.markdown {
+		:global {
+			@import '../styles/markdown.scss';
+		}
+	}
+</style>
