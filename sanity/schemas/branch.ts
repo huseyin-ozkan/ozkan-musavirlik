e10e011052efc94967ad92032af7666047b851e0
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-19 20:22:28 +0300

    feat(sanity): branch schema type

---
+++ sanity/schemas/branch.ts
@@ -0,0 +1,47 @@
+// TODO address
+// TODO map link
+// TODO phones
+// TODO email
+
+import {defineType} from 'sanity'
+
+const branch = defineType({
+  name: 'branch',
+  title: 'Branch',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required().min(4).max(20),
+    },
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+      validation: (Rule) => Rule.required().min(10).max(100),
+    },
+    {
+      name: 'mapLink',
+      title: 'Map Link',
+      type: 'string',
+      validation: (Rule) => Rule.uri(),
+    },
+    {
+      name: 'phones',
+      title: 'Phones',
+      type: 'array',
+      of: [{type: 'string'}],
+      validation: (Rule) => Rule.required().min(1).max(2),
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      validation: (Rule) => Rule.required().email(),
+    },
+  ],
+})
+
+export default branch
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,5 +2,6 @@ import hero from './hero'
 import post from './post'
 import about from './about'
 import staff from './staff'
+import branch from './branch'
 
-export const schemaTypes = [hero, post, about, staff]
+export const schemaTypes = [hero, post, about, staff, branch]
