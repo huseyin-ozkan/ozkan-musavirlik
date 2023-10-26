90b2573c5050ac650bebd0762ab40ad1fbc63b3c
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-10-26 19:35:15 +0300

    feta: Services icon better responsive size

--- src/routes/Services.svelte
+++ src/routes/Services.svelte
@@ -63,7 +63,7 @@
 		--desc-width: calc(var(--title-font-size) * 10);
 
 		--service-width: calc(var(--title-font-size) * 10);
-		--icon-width: clamp(3rem, 5vw, 5rem);
+		--icon-width: clamp(50px, 5vw, 100px);
 
 		--title-color: var(--color-text-1);
 		--description-color: var(--color-text-2);
